df8771f9aec79f98512d6f8400d60f65234067f9
AO Huijun <daca.hjao@gmail.com>
2015-03-13 01:37:21 +0800

    <version>v0.1.01</version> <author>AO Huijun</author> <desc> 	modified:   AndroidManifest.xml 	modified:   res/layout/activity_main_page.xml 	new file:   res/layout/activity_start_page.xml 	modified:   res/layout/setting_item.xml 	modified:   res/values/strings.xml 	new file:   src/com/shyer/main/StartPage.java </desc>

---
+++ src/com/shyer/main/StartPage.java
@@ -0,0 +1,13 @@
+package com.shyer.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartPage extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_start_page);
+	}
+}
